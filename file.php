16f9ee1cfae51915877d95033ee7c571e0ecbbf3
tomek <tmietkiewicz@gmail.com>
2015-09-18 10:51:21 +0200

    nic

---
+++ file.php
@@ -0,0 +1,21 @@
+<pre>
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+
+
+$a = 5;
+$tekst = 'jakistekst';
+
+echo gettype($a);
+
+echo '</br>';
+
+echo gettype($tekst);
+
+
+
+
+?>
+
