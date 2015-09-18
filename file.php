7352199688bd6716a38cb90bfc6e5e0cf95cb238
tomek <tmietkiewicz@gmail.com>
2015-09-18 16:17:34 +0200

    nic

--- file.php
+++ file.php
@@ -4,18 +4,99 @@
 error_reporting(E_ALL);
 ini_set('display_errors',1);
 
+//$tab = array(
+//    'Ania'=> array(
+//        'biurko',
+//        'kubek',
+//    ),
+//    'Kasia'=>array(
+//        'biurko',
+//        'kubek',
+//    )
+//
+//);
+//
+//
+//
+//$a = 5.0;
+//$tekst = 'jakistekst';
 
-$a = 5;
-$tekst = 'jakistekst';
+//echo gettype($a);
+//
+//echo '</br>';
+//
+//echo gettype($tekst);
 
-echo gettype($a);
+//settype($a,'string');
+//echo '</br>';
+//echo gettype($a);
 
-echo '</br>';
-
-echo gettype($tekst);
+//echo is_float($a)
 
 
+//$x = "123 ala ma kota 4567";
+//echo(int)$x;
+
+//var_export($tab);
+//print_r($tab);
+
+//$dodawanie =1;
+//echo $dodawanie++;
+//echo '</br>';
+//echo ++$dodawanie;
+
+
+//$biurka=array(
+//    'biurko1',
+//    'biurko2',
+//    'biurko3',
+//    'biurko4',
+//    'biurko5',
+//    'biurko6',
+//    'biurko7',
+//    'biurko8',
+//);
+
+
+//for ($i=0; $i<sizeof($biurka); $i++){
+//echo $biurka[$i].'</br>';
+//}
+
+//foreach ($biurka as $key =>$val){
+//    echo $key."->".$val.'</br>';
+//}
+
+
+//$x=5;
+//echo $x;
+//
+//
+//include('file_two.php');
+//echo'</br>';
+//print_r($_GET)
 
 
 ?>
+    <html lang="pl">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="Taskie - Task Manager">
+        <meta name="author" content="Pied Piper">
+        <meta name="keywords" content="taskmanager, task, manager">
+        <title>Pied Piper TASKie</title>
+        <link rel="stylesheet" href="styles/styles.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Josefin+Sans:600' rel='stylesheet' type='text/css'>
+        <meta http-equiv="Cache-Control" content="no-store, no-cache, must-revalidate" />
+
+    </head>
+    <body>
+    <form action="file_two.php" method="GET">
+        <input type="text" name="pole1" value="cos">
+        <input type="text" name="pole2" value="xxx">
+        <input type="submit" name="submit" value="submit">
+    </form>
+
+    </body>
 
