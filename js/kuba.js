a3e6f75b994958dad6e3767803cb7f774268b77c
jakubmatyka <jaqbec@gmail.com>
2015-09-22 15:44:34 +0200

    porządki w html i css

--- js/kuba.js
+++ js/kuba.js
@@ -7,4 +7,52 @@ function formvalidation(){
         alert("Brak małpy!");
     }
 }
+function clickfcn(){
+    console.log(12);
+    $('.thank_you').show();
+    $('#newsletter').submit();
+}
+
+$(document).ready(function () {
+    $('.thank_you').hide();
+    $('#zapisz_sie').click(clickfcn());
+});
+
+$(document).ready(function(){
+    $('#icancel').click(function(){
+        $('#formularz').hide(function(){
+            $(this).animate({
+            },2000)
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#cancel').click(function(){
+        $('.registration').hide(function(){
+            $(this).animate({
+            },2000)
+        });
+        $('#lightbox').hide(function(){
+            $(this).animate({
+            },2000)
+        });
+    });
+});
+
+$('.registration').hide();
+$('#lightbox').hide();
+
+$(document).ready(function(){
+    $('#zapisz_header').click(function(){
+        $('.registration').show(function(){
+            $(this).animate({
+            },2000)
+        });
+        $('#lightbox').show(function(){
+            $(this).animate({
+            },2000)
+        });
+    });
+});
 
