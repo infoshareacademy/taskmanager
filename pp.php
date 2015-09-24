0e2b265e94396bdecf6a9596ef71054210b563f9
tomek <tmietkiewicz@gmail.com>
2015-09-24 17:02:39 +0200

    TM-20 dodany warunek sprawdzania czy uzupełnione są pola email i imię. Nie robimy teraz połączenia do bazy z pustymi danymi. Dodatkowo zamknięcie połączenia po dodaniu elementu do bazy

--- pp.php
+++ pp.php
@@ -1,14 +1,22 @@
 <?php
-$conn = mysqli_connect("sql.infoshareaca.nazwa.pl", 'infoshareaca_3', 'iSAf0r3v3r', "infoshareaca_3", 3307);
-if (!$conn) {
-    die ('connection failed: '.mysqli_connect_errno());
+if($_POST['imie']&&$_POST['email']) {
+
+    $conn = mysqli_connect("sql.infoshareaca.nazwa.pl", 'infoshareaca_3', 'iSAf0r3v3r', "infoshareaca_3", 3307);
+    if (!$conn) {
+        die ('connection failed: ' . mysqli_connect_errno());
+    }
+
+
+
+    $sql = "INSERT INTO `tabela_newsletter`(`ID`, `imie`, `email`) VALUES (null,'" . $_POST['imie'] . "','" . $_POST['email'] . "')";
+
+    mysqli_query($conn, $sql);
+
+    mysqli_close($conn);
 }
-//print_r($_POST);
 
-$sql = "INSERT INTO `tabela_newsletter`(`ID`, `imie`, `email`) VALUES (null,'".$_POST['imie']."','".$_POST['email']."')";
-
-mysqli_query($conn, $sql);
-//echo $sql;
 
 header("location: index.html");
+
+
 ?>
