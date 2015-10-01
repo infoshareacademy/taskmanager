43a17f1c74b8768a21e4e5af3e243d9bc4146e64
tomek <tmietkiewicz@gmail.com>
2015-10-01 17:00:57 +0200

    Do Bartosza, post z js na formularzu newsleterra

--- pp.php
+++ pp.php
@@ -1,8 +1,7 @@
 <?php
 if (empty($_POST['imie'] && $_POST['email'])) {
-    $fillAllForms = 'Musisz wypelnic wszystkie pola!';
-    echo "<script type='text/javascript'>alert('$fillAllForms');window.location.pathname='/taskmanager/index.html';</script>";
-    mysqli_close($conn);
+    echo 'Musisz wypelnic wszystkie pola!';
+
 
 } else {
     $conn = mysqli_connect("sql.infoshareaca.nazwa.pl", 'infoshareaca_3', 'iSAf0r3v3r', "infoshareaca_3", 3307);
@@ -16,7 +15,7 @@ if (empty($_POST['imie'] && $_POST['email'])) {
     $emailExist = mysqli_fetch_array($emailDB, MYSQLI_NUM);
 
     if ($emailExist[0] > 1) {
-        echo "Twoj email znajduje sie juz w bazie, nie zostales ponownie dodany<br/>";
+        echo "Twoj email znajduje sie juz w bazie, nie zostales ponownie dodany";
         mysqli_close($conn);
     } else {
         $insertQuery = "INSERT INTO `tabela_newsletter`(`ID`, `imie`, `email`) VALUES (NULL,'" . $_POST['imie'] . "','" . $_POST['email'] . "')";
@@ -25,9 +24,9 @@ if (empty($_POST['imie'] && $_POST['email'])) {
         if (mysqli_query($conn, $insertQuery)) {
             mysqli_query($conn, $insertQuery);
             mysqli_close($conn);
-            echo "Dziekujemy za zgloszenie do newslettera!<br/>
-                  Zostales zarejestrowany i będziesz otrzymywał nasze wiadomosci<br/>
-                  Twoje dane:<br/>
+            echo "Dziekujemy za zgloszenie do newslettera!
+                  Zostales zarejestrowany i będziesz otrzymywał nasze wiadomosci
+                  Twoje dane:
                     <ul>
                         <li>Imie:$_POST[imie];</li>
                         <li>Adres email: $_POST[email];</li>
@@ -36,6 +35,3 @@ if (empty($_POST['imie'] && $_POST['email'])) {
 
     }
 }
-
-?>
-
