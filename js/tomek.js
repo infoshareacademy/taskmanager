ff69781980e8b9bbb7c7164dc6f0a77f76df0674
tomek <tmietkiewicz@gmail.com>
2015-10-01 17:00:57 +0200

    Do Bartosza, post z js na formularzu newsleterra

--- js/tomek.js
+++ js/tomek.js
@@ -13,3 +13,29 @@ function changePic() {
 
 
 setInterval(changePic,1500);
+
+
+$(function () {
+    $('#newsletter').on('submit',function (event) {
+        var $form = $(this);
+        // get all the inputs into an array.
+        var $inputs = $form.find(':input');
+
+        var values = {};
+        $inputs.each(function() {
+            values[this.name] = $(this).val();
+        });
+        var $logInStatusContainer = $('#submit-status');
+
+        // get users.json to lookup for username and password
+        $.post('/taskmanager/pp.php', values,function (message) {
+
+                $logInStatusContainer.text(message);
+        });
+        $logInStatusContainer.text('Checking...');
+
+        //console.log(values);
+
+        return false;
+    });
+});
