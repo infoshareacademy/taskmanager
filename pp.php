da4fe7909d9e1aaeba8fe7aea916c66096f8d773
tomek <tmietkiewicz@gmail.com>
2015-10-01 13:40:30 +0200

    TM-20: poprawione warunki połączenia i sprawdzania w PHP. Dla warunku musisz uzupełnić wszystkie pola dodano alert JS, pozostałe komunikatem z funkcji echo. Inne: Zmiana grafik na dwie żarówki.

--- pp.php
+++ pp.php
@@ -1,34 +1,28 @@
 <?php
-if (empty($_POST['imie']&&$_POST['email']))
-//if($_POST['imie']&&$_POST['email'])
-    {
-        echo "<p style=\"color:red\">Musisz wypelnic wszystkie pola!</p>";}
-else{
+if (empty($_POST['imie'] && $_POST['email'])) {
+    $fillAllForms = 'Musisz wypelnic wszystkie pola!';
+    echo "<script type='text/javascript'>alert('$fillAllForms');window.location.pathname='/taskmanager/index.html';</script>";
+    mysqli_close($conn);
+
+} else {
     $conn = mysqli_connect("sql.infoshareaca.nazwa.pl", 'infoshareaca_3', 'iSAf0r3v3r', "infoshareaca_3", 3307);
 
-    if (!$conn)
-    {
+    if (!$conn) {
         die ('connection failed: ' . mysqli_connect_errno());
     }
 
-    $check="SELECT * FROM tabela_newsletter WHERE email = '$_POST[email]'";
-    $emailDB = mysqli_query($conn,$check);
+    $check = "SELECT * FROM tabela_newsletter WHERE email = '$_POST[email]'";
+    $emailDB = mysqli_query($conn, $check);
     $emailExist = mysqli_fetch_array($emailDB, MYSQLI_NUM);
 
-    if($emailExist[0] > 1)
-    {
+    if ($emailExist[0] > 1) {
         echo "Twoj email znajduje sie juz w bazie, nie zostales ponownie dodany<br/>";
-    }
-
-        else
-    {
-        $insertQuery= "INSERT INTO `tabela_newsletter`(`ID`, `imie`, `email`) VALUES (null,'" . $_POST['imie'] . "','" . $_POST['email'] . "')";
+        mysqli_close($conn);
+    } else {
+        $insertQuery = "INSERT INTO `tabela_newsletter`(`ID`, `imie`, `email`) VALUES (NULL,'" . $_POST['imie'] . "','" . $_POST['email'] . "')";
 
 
-
-
-        if (mysqli_query($conn,$insertQuery))
-        {
+        if (mysqli_query($conn, $insertQuery)) {
             mysqli_query($conn, $insertQuery);
             mysqli_close($conn);
             echo "Dziekujemy za zgloszenie do newslettera!<br/>
